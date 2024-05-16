c2da999e6860cda9d75488d201553de2fc648158
ahmed-jobayer <jobayer320721@.com>
2024-05-16 21:50:10 +0600

    all service load functionality done

--- src/Pages/All Services/AllServices.jsx
+++ src/Pages/All Services/AllServices.jsx
@@ -1,19 +1,33 @@
+import { useEffect, useState } from "react";
 import Footer from "../Shared/Footer/Footer";
 import Navbar from "../Shared/Navbar/Navbar";
 import SingleService from "./SingleService";
 
-
 const AllServices = () => {
-    return (
-        <div className="container mx-auto">
-            <Navbar></Navbar>
-            <div className="my-6">
-                <h2 className="text-center my-6 text-3xl">All Services</h2>
-            <SingleService></SingleService>
-            </div>
-            <Footer></Footer>
-        </div>
-    );
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/services")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
+
+  //   console.log(services)
+
+  return (
+    <div className="container mx-auto">
+      <Navbar></Navbar>
+      <div className="my-6 flex flex-col gap-10">
+        <h2 className="text-center my-6 text-3xl">All Services</h2>
+        {services.map((service) => (
+          <SingleService 
+          key={service._id} 
+          service={service}></SingleService>
+        ))}
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default AllServices;
--- src/Pages/All Services/SingleService.jsx
+++ src/Pages/All Services/SingleService.jsx
@@ -1,29 +1,32 @@
 import { Link } from "react-router-dom";
 
-const SingleService = () => {
+const SingleService = ({service}) => {
+
+    
+
   return (
     <div className="card lg:card-side bg-base-100 shadow-xl lg:max-w-6xl mx-auto">
       <figure>
         <img
-          src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg"
+          src={service.imgURL}
           alt="Album"
         />
       </figure>
       <div className="card-body">
-        <h2 className="card-title">New album is released!</h2>
-        <p>Click the button to listen on Spotiwhy app.</p>
+        <h2 className="card-title">{service.serviceName}</h2>
+        <p>{service.description}</p>
         <div className="my-4">
-          <h4 className="mb-2">Service Area</h4>
-          <h4>Service Price</h4>
+          <h4 className="mb-2">{service.serviceArea}</h4>
+          <h4>{service.Price}</h4>
         </div>
         <div className="flex justify-between">
           <div className="flex items-center gap-3">
             <div className="avatar">
               <div className="w-14 rounded-xl">
-                <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                <img src={service.providerImage} />
               </div>
             </div>
-            <h3>name</h3>
+            <h3>{service.providerName}</h3>
           </div>
           <div className="card-actions justify-end">
             <Link to="/serviceDetails">
