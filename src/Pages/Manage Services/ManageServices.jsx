5ea90aea7653d614e5c0dcce4805f8857af21403
ahmed-jobayer <jobayer320721@.com>
2024-05-30 02:09:25 +0600

    user wise booked service page functionaly added

--- src/Pages/Manage Services/ManageServices.jsx
+++ src/Pages/Manage Services/ManageServices.jsx
@@ -1,22 +1,32 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Footer from "../Shared/Footer/Footer";
 import Navbar from "../Shared/Navbar/Navbar";
 import { FaEdit } from "react-icons/fa";
 import { MdDeleteForever } from "react-icons/md";
 import axios from "axios";
 import Swal from "sweetalert2";
+import { AuthContext } from "../../Providers/AuthProviders";
 
 const ManageServices = () => {
   const [bookedServises, setBookedServices] = useState([]);
 
+  const { user } = useContext(AuthContext);
+  const currentUserEmail = user.email;
+  // console.log(currentUserEmail)
   // const {_id, serviceName, providerName, price } = bookedServises
 
   useEffect(() => {
-    axios.get("http://localhost:5000/bookedServices").then((data) => {
-      setBookedServices(data.data);
-      // console.log(data.data)
-    });
-  }, []);
+    axios
+      .get("http://localhost:5000/bookedServices", {
+        params: {
+          currentUserEmail: currentUserEmail,
+        },
+      })
+      .then((data) => {
+        setBookedServices(data.data);
+        // console.log(data.data)
+      });
+  }, [currentUserEmail]);
 
   // console.log(_id, serviceName, providerName, price)
 
@@ -37,11 +47,16 @@ const ManageServices = () => {
           .then((data) => {
             console.log(data.data);
             if (data.data.deletedCount > 0) {
+              const remaining = bookedServises.filter(
+                (bookedService) => bookedService._id !== _id
+              );
+
               Swal.fire({
                 title: "Deleted!",
                 text: "Your file has been deleted.",
                 icon: "success",
               });
+              setBookedServices(remaining);
             }
           })
           .catch((error) => {
@@ -55,43 +70,52 @@ const ManageServices = () => {
       }
     });
   };
+  // console.log(bookedServises.length)
 
   return (
     <div className="container mx-auto">
       <Navbar></Navbar>
-      <div className="overflow-x-auto min-h-screen relative z-40 my-8">
-        <table className="table">
-          {/* head */}
-          <thead>
-            <tr className="text-xl">
-              <th>Service Name</th>
-              <th>Provider Name</th>
-              <th>Price</th>
-              <th>Update</th>
-              <th>Delete</th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* row */}
-            {bookedServises.map((bookedService) => (
-              <tr key={bookedService._id} className="hover">
-                <td>{bookedService.serviceName}</td>
-                <td>{bookedService.providerName}</td>
-                <td>{bookedService.price}</td>
-                <td>
-                  <FaEdit className="text-xl cursor-pointer" />
-                </td>
-                <td>
-                  <MdDeleteForever
-                    onClick={() => handleDelete(bookedService._id)}
-                    className="text-2xl cursor-pointer"
-                  />
-                </td>
+      {bookedServises.length < 1 ? (
+        <>
+          <div className="min-h-screen flex items-center justify-center">
+            <h3>You did not booked any services yet</h3>
+          </div>
+        </>
+      ) : (
+        <div className="overflow-x-auto min-h-screen relative z-40 my-8">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr className="text-xl">
+                <th>Service Name</th>
+                <th>Provider Name</th>
+                <th>Price</th>
+                <th>Update</th>
+                <th>Delete</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+            </thead>
+            <tbody>
+              {/* row */}
+              {bookedServises.map((bookedService) => (
+                <tr key={bookedService._id} className="hover">
+                  <td>{bookedService.serviceName}</td>
+                  <td>{bookedService.providerName}</td>
+                  <td>{bookedService.price}</td>
+                  <td>
+                    <FaEdit className="text-xl cursor-pointer" />
+                  </td>
+                  <td>
+                    <MdDeleteForever
+                      onClick={() => handleDelete(bookedService._id)}
+                      className="text-2xl cursor-pointer"
+                    />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
       <Footer></Footer>
     </div>
   );
